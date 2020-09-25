12de18715a4efa380aaa309803c845a75a3d2c6d
AryehSchoefer <aryeh.shmuel@googlemail.com>
2020-09-25 15:21:36 +0200

    added: codemirror editor; typing functionality in editor

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Editor from './Editor'
 
 function App() {
-	return <h1>Hello World!</h1>
+	const [html, setHtml] = useState('')
+	const [css, setCss] = useState('')
+	const [javascript, setJavascript] = useState('')
+
+	return (
+		<>
+			<div className="pane upper-pane">
+				<Editor language="xml" displayName="HTML" value={html} onChange={setHtml} />
+				<Editor language="css" displayName="CSS" value={css} onChange={setCss} />
+				<Editor language="javascript" displayName="JS" value={javascript} onChange={setJavascript} />
+			</div>
+			<div className="pane">
+				<iframe title="output" sandbox="allow-scripts" frameBorder="0" width="100%" height="100%" />
+			</div>
+		</>
+	)
 }
 
 export default App
