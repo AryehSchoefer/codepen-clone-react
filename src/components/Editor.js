27168f64e6d945028d17b32f6d413c243e729d04
AryehSchoefer <aryeh.shmuel@googlemail.com>
2020-09-25 19:39:15 +0200

    added: open/close icon; custom hook for saving code on localStorage; some styles

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import 'codemirror/theme/material.css'
 import 'codemirror/lib/codemirror.css'
 // syntax-highlighting:
@@ -7,19 +7,26 @@ import 'codemirror/mode/javascript/javascript'
 import 'codemirror/mode/css/css'
 // import text-editor:
 import { Controlled as ControlledEditor } from 'react-codemirror2'
+// fontawesome icons
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCompressAlt, faExpandAlt } from '@fortawesome/free-solid-svg-icons'
 
 export default function Editor(props) {
 	const { language, displayName, value, onChange } = props
+
+	const [open, setOpen] = useState(true)
 
 	const handleChange = (editor, data, value) => {
 		onChange(value)
 	}
 
 	return (
-		<div className="editor-container">
+		<div className={`editor-container ${open ? '' : 'collapsed'}`}>
 			<div className="editor-title">
 				{displayName}
-				<button>O/C</button>
+				<button type="button" className="expand-collapse-btn" onClick={() => setOpen((prevOpen) => !prevOpen)}>
+					<FontAwesomeIcon icon={open ? faCompressAlt : faExpandAlt} />
+				</button>
 			</div>
 			<ControlledEditor
 				className="code-mirror-wrapper"
